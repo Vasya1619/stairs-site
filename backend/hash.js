08535f9af4489b3c9d36b75298974e5967d7a41b
Vasya1619 <volosatiymamont@gmail.com>
2026-05-04 18:13:41 +0300

    Delete backend directory

--- backend/hash.js
+++
@@ -1,7 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const password = 'admin0303';  // поменяй на свой пароль
-
-bcrypt.hash(password, 10).then(hash => {
-    console.log('Хеш пароля:', hash);
-});
