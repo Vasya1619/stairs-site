48ae97ca80136b4d6c627085cab50cdd76bea88a
Vasya1619 <volosatiymamont@gmail.com>
2026-05-04 19:52:03 +0300

    Delete public directory

--- public/JS/admin.js
+++
@@ -1,9 +0,0 @@
-
-document.querySelectorAll(".btn-image").forEach(button => {
-  button.addEventListener("click", () => {
-    const galleryId = button.dataset.gallery;
-    const gallery = document.getElementById(galleryId);
-
-    gallery.classList.toggle("active");
-  });
-});
