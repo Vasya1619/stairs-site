cb7a298ad931896d9ce20fc43fa77359f51bbb4d
Vasya1619 <volosatiymamont@gmail.com>
2026-05-12 23:05:12 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,8 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 const PORT = process.env.PORT || 5000;;
-const DB_URL = 'mysql://root@localhost:3306/stairs'
-const connection = await mysql.createPool(DB_URL)
+const DB_URL = 'mysql://root@127.0.0.1:3306/stairs';
+const connection = mysql.createPool(DB_URL);
 const JWT_SECRET = 'mySecretKey123';
 
 app.use(express.json());
